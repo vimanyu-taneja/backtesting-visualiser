655eb27fdf40f57f02e28feea87b114d0d2736bc
Vimanyu Taneja <vimanyu.taneja@hotmail.com>
2023-08-11 02:58:59 +0100

    Accept POST request using form

--- routes/views.py
+++ routes/views.py
@@ -1,8 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 views = Blueprint("views", __name__)
 
 
-@views.route("/")
+@views.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+    if request.method == "POST":
+        data = request.form
+        ticker = request.form.get("ticker")
+    return render_template("index.html", plot=ticker)
