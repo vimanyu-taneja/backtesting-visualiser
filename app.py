f458888f5cf197becc5aa0210c891bae542f218d
Vimanyu Taneja <vimanyu.taneja@hotmail.com>
2023-08-11 03:30:56 +0100

    Debug flag should be boolean

--- app.py
+++ app.py
@@ -14,4 +14,4 @@ app.register_blueprint(api, url_prefix="/api")
 app.register_blueprint(views, url_prefix="/")
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=os.getenv("FLASK_ENV"))
+    app.run(host="127.0.0.1", port=5000, debug=bool(os.getenv("FLASK_ENV")))
