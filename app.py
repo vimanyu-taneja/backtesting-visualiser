d15617bbaf47b84c15479501d63f4c28fcda3aaa
Vimanyu Taneja <vimanyu.taneja@hotmail.com>
2023-08-11 21:29:43 +0100

    Keep alternative initialisation code as a backup

--- app.py
+++ app.py
@@ -21,3 +21,7 @@ def generate_plot():
     filename = "/tmp/plot.html" if os.getenv("VERCEL") else "plot.html"
     plotter.generate_plot(filename=filename)
     return plotter.test()
+
+
+if __name__ == "__main__" and os.getenv("FLASK_ENV"):
+    app.run(host="127.0.0.1", port=8000, debug=True)
