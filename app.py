58d900ada7e08d466a56317009aabb067d34d89f
Vimanyu Taneja <vimanyu.taneja@hotmail.com>
2023-08-11 03:17:54 +0100

    Get debug status from environment variable

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
+import os
+
+from dotenv import load_dotenv
 from flask import Flask
 
 from routes.api import api
 from routes.views import views
+
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -9,4 +14,4 @@ app.register_blueprint(api, url_prefix="/api")
 app.register_blueprint(views, url_prefix="/")
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="127.0.0.1", port=5000, debug=os.getenv("FLASK_ENV"))
