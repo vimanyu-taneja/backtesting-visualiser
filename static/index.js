65f2c702ef9a255ea0c58333d2bdb4ff661f55c5
Vimanyu Taneja <vimanyu.taneja@hotmail.com>
2023-08-12 19:11:21 +0100

    Show loading spinner when plot is being updated

--- static/index.js
+++ static/index.js
@@ -14,6 +14,7 @@ $(document).ready(function () {
 });
 
 $(document).on("submit", "#params-form", function (event) {
+  $("#plot-container").html("<div class='loading'></div>");
   event.preventDefault();
   updateBacktestResults();
 });
