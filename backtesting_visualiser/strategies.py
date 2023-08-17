9a8c50cd8af39a95162e08ebbc72b5f5ca243754
Vimanyu Taneja <vimanyu.taneja@hotmail.com>
2023-08-17 01:30:07 +0100

    Add RSI Oscillator strategy

--- backtesting_visualiser/strategies.py
+++ backtesting_visualiser/strategies.py
@@ -4,6 +4,21 @@ from backtesting import Strategy
 from backtesting.lib import crossover
 
 
+class RSIOscillator(Strategy):
+    upper_bound = 70
+    lower_bound = 30
+
+    def init(self):
+        close = self.data.Close
+        self.rsi = self.I(ta.momentum.rsi, pd.Series(close), 14)
+
+    def next(self):
+        if crossover(self.lower_bound, self.rsi):
+            self.buy()
+        elif crossover(self.rsi, self.upper_bound):
+            self.sell()
+
+
 class SMACross(Strategy):
     n1 = 50
     n2 = 100
@@ -21,5 +36,5 @@ class SMACross(Strategy):
 
 
 def get_strategy(name):
-    strategies = {"SMA crossover": SMACross, "RSI oscillator": SMACross}
+    strategies = {"RSI oscillator": RSIOscillator, "SMA crossover": SMACross}
     return strategies[name]
